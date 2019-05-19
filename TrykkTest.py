2058a23c4c2040f6d78e58cc61c8960533685901
Kjetil <brekkis90@gmail.com>
2019-05-19 23:36:13 +0200

    Fixed inn_source and level/pressure transmitter. Removed clutter

--- TrykkTest.py
+++ TrykkTest.py
@@ -13,9 +13,9 @@ screen = pygame.display.set_mode((wScreen, hScreen))
 class Separator(object):
     pressure = 0
     level = 0
-    flowInn = 5000
-    volumeGas = 8
-    volumeLef = 0
+    levelActual = 0
+    volumeGas = 0
+    volumeLeft = 0
 
     def __init__(self, tag, inn_source, x=100, y=100, volume=8):
         self.width = 100
@@ -25,18 +25,27 @@ class Separator(object):
         self.volume = volume
         self.tag = tag
         self.inn_source = inn_source
+        self.flowInnGas = self.inn_source[1]
+        self.flowInnOil = self.inn_source[0]
 
     def draw(self):
         pygame.draw.rect(screen, (65, 65, 65), (self.x, self.y, self.width, self.height))
-        self.volumeLef = self.volume
-        self.volumeGas = self.volumeGas + self.flowInn / m3h
-        self.pressure = self.volumeGas / self.volumeLef
+        self.levelActual = self.levelActual + self.flowInnOil / m3h
+        self.volumeLeft = self.volume - self.levelActual
+        self.volumeGas = self.volumeGas + self.flowInnGas / m3h
+
+        self.pressure = self.volumeGas / self.volumeLeft
+        if self.levelActual < self.volume:
+            self.level = self.levelActual / self.volume
+        else:
+            self.levelActual = self.volume
+            self.volumeLeft = 0
 
 
 class Transmitter(object):
-    def __init__(self, typ, tag, x=100, y=100):
-        self.width = 10
-        self.height = 10
+    def __init__(self, typ, tag, x=200, y=200):
+        self.width = 100
+        self.height = 50
         self.x = x
         self.y = y
         self.tag = tag
@@ -49,44 +58,26 @@ class Transmitter(object):
             content = font.render('Bar: ' + str(round(measuring_point, 2)), 1, (255, 255, 255))
             screen.blit(content, (self.x, self.y))
         elif self.typ == 'level':
-            content = font.render('Level: ' + str(round(measuring_point / volume * 100, 2)) + '%', 1, (255, 255, 255))
+            content = font.render('Level: ' + str(round(measuring_point * 100, 2)) + '%', 1, (255, 255, 255))
             screen.blit(content, (self.x, self.y))
         else:
             content = font.render('fault: ' + str(round(measuring_point, 2)) + '%', 1, (255, 255, 255))
             screen.blit(content, (self.x, self.y))
 
 
-'''def transmitter(unit, x, y):
-    font = pygame.font.SysFont('arial', 30, True)
-    if unit == pressure:
-        content = font.render('Bar: ' + str(round(unit, 2)), 1, (255, 255, 255))
-        screen.blit(content, (x, y))
-    elif unit == level:
-        content = font.render('Level: ' + str(round(unit / volume * 100, 2)) + '%', 1, (255, 255, 255))
-        screen.blit(content, (x, y))'''
-
-
-d001 = Separator('d001', 100, 500, 10)
-pi001 = Transmitter('pressure', 'pi001', d001.pressure)
+d001 = Separator('d001', (5000, 5000), 500, 10)
+pi001 = Transmitter('pressure', 'pi001')
+li001 = Transmitter('level', 'li001', 500, 500)
 
 
 def redraw():
     screen.fill((128, 128, 128))
-    #transmitter(pressure, 100, 300)
-    #transmitter(level, 100, 100)
     d001.draw()
     pi001.draw(d001.pressure)
+    li001.draw(d001.level)
     pygame.display.update()
+    print(d001.volumeLeft)
 
-
-# Pressure and simulation
-flowGas = 1000
-flowLiquid = 500
-volume = 8
-volumeGas = 8
-pressure = 0
-level = 0
-volumeLeft = volume
 
 clock = pygame.time.Clock()
 fps = 30
@@ -103,12 +94,6 @@ while run:
         elif event.type == pygame.MOUSEBUTTONUP:
             clicked = False
 
-    # Pressure and simulation
-    volumeGas = volumeGas + flowGas / m3h
-    pressure = volumeGas / volumeLeft
-    level = level + flowLiquid / m3h / volume
-    volumeLeft = volume - level
-    print(d001.pressure)
     redraw()
 
 pygame.quit()
