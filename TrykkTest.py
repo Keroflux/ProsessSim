7b6a82d559417bd6d199aa40eb609798c61629e3
Kjetil <brekkis90@gmail.com>
2019-05-22 22:59:03 +0200

    Major fixes and fuckups for flow inn

--- TrykkTest.py
+++ TrykkTest.py
@@ -8,11 +8,12 @@ pygame.init()
 wScreen = 1000
 hScreen = 800
 screen = pygame.display.set_mode((wScreen, hScreen), pygame.RESIZABLE)
-
+fps = 30
+m3h = 3600 * fps
 
 class Separator(object):
 
-    def __init__(self, inn_source, x=100, y=100, volume=8, out_source=0):
+    def __init__(self, x=100, y=100, volume=8):
         self.volumeLeft = 0
         self.pressure = 0
         self.volumeGas = 0
@@ -26,18 +27,14 @@ class Separator(object):
         self.y = y
         self.volume = volume
 
-        self.inn_source = inn_source
-        self.out_source = out_source
-
-        self.flowInnOil = self.inn_source[0]
-        self.flowInnGas = self.inn_source[1]
-        self.flowInnWater = self.inn_source[2]
-
-    def draw(self):
+    def draw(self, oil_inn, gas_inn, water_inn, out_source):
         pygame.draw.rect(screen, (85, 85, 85), (self.x, self.y, self.width, self.height))
-        self.levelCubes = self.levelCubes + self.flowInnOil / m3h
+        flow_inn_oil = oil_inn.flow * m3h
+        flowInnGas = gas_inn.flow * m3h
+        flowInnWater = water_inn.flow
+        self.levelCubes = self.levelCubes - out_source.flow + flow_inn_oil / m3h
         self.volumeLeft = self.volume - self.levelCubes
-        self.volumeGas = self.volumeGas + self.flowInnGas / m3h
+        self.volumeGas = self.volumeGas + flowInnGas / m3h
 
         self.pressure = self.volumeGas / self.volumeLeft
         self.level = self.levelCubes / self.volume * 100
@@ -58,8 +55,10 @@ class Transmitter(object):
             content = font.render('Pressure: ' + str(round(measuring_point.pressure, 2)) + 'Bar', 1, (255, 255, 255))
             screen.blit(content, (self.x, self.y))
         elif self.typ == 'level':
-            content = font.render('Level: ' + str(round(measuring_point.level, 2)) + '%', 1, (255, 255, 255))
-            screen.blit(content, (self.x, self.y))
+            content = font.render('Level: ', 1, (255, 255, 255))
+            value = font.render(str(round(measuring_point.level, 2)) + '%', 1, (255, 255, 255))
+            screen.blit(content, (self.x + 5, self.y))
+            screen.blit(value, (self.x + 5, self.y + 20))
         elif self.typ == 'flow':
             content = font.render(str(round(measuring_point.flow * m3h, 2)) + 'm3/h', 1, (255, 255, 255))
             screen.blit(content, (self.x, self.y))
@@ -69,7 +68,7 @@ class Transmitter(object):
 
 
 class Valve(object):
-    def __init__(self, typ, size=10, x=200, y=200):
+    def __init__(self, typ, size=2, x=200, y=200):
         self.width = 100
         self.height = 50
         self.opening = 10
@@ -79,31 +78,45 @@ class Valve(object):
         self.x = x
         self.y = y
 
-    def draw(self, measuring_point):
+    def draw(self, source):
         pygame.draw.rect(screen, (5, 5, 5), (self.x, self.y, self.width, self.height))
-        self.flow = self.size * measuring_point.pressure * self.opening / m3h
+        self.flow = self.size * source.pressure * self.opening / m3h
 
 
-d001 = Separator((500, 5000, 0), 500, 10)
+class Dummy(object):
+    def __init__(self, flow, pressure):
+        self.flow = flow
+        self.pressure = pressure
+
+
+dummy = Dummy(0, 0)
+dummy2 = Dummy(50 / m3h, 0)
+dummy3 = Dummy(5000 / m3h, 0)
+d001 = Separator(500, 10)
 pi001 = Transmitter('pressure')
 li001 = Transmitter('level', 500, 500)
-fv001 = Valve('flow', 10, 500, 600)
+fv001 = Valve('flow', 2, 500, 600)
 fi001 = Transmitter('flow', 500, 650)
+d002 = Separator(100, 500)
+li002 = Transmitter('level', 400, 750)
 
 
 def redraw():
     screen.fill((128, 128, 128))
-    d001.draw()
+
     pi001.draw(d001)
     li001.draw(d001)
     fv001.draw(d001)
     fi001.draw(fv001)
+    d001.draw(dummy2, dummy3, dummy2, fv001)
+    d002.draw(fv001, dummy2, dummy, dummy)
+    li002.draw(d002)
+
     pygame.display.update()
 
 
 clock = pygame.time.Clock()
-fps = 30
-m3h = 3600 * fps
+
 run = True
 while run:
     clock.tick(fps)
