209b11308929104b933bf0315f8edf994dfdca12
Kjetil <brekkis90@gmail.com>
2019-06-12 16:54:37 +0200

    Testing sgc, not going well

--- buttons.py
+++ buttons.py
@@ -1,19 +1,94 @@
 import pygame
-
+import sgc
+from sgc.locals import *
 
 pygame.init()
+wScreen = 1200
+hScreen = 800
+
+screen = pygame.display.set_mode((wScreen, hScreen), pygame.RESIZABLE)
+#screen = sgc.surface.Screen((640,480))
+
+userFPS = 30
+clock = pygame.time.Clock()
+pause = False
+clicked = False
+rClicked = False
+lClicked = False
+edit = False
+clickCount = 0
+
+#btn = sgc.Button(label='click', pos=(100, 200))
+#btn.add(0)
 
 
-def is_mouse_inside(x, y, width, height):
-    m_pos = pygame.mouse.get_pos()
-    if x < m_pos[0] < x + width and y < m_pos[1] < y + height:
-        return True
 
 
-def button(x, y, width, height, screen, msg='BUTTON', func=None):
 
-    pygame.draw.rect(screen, (0, 100, 0), (x, y, width, height))
-    font = pygame.font.SysFont('arial', 25, True)
-    opening = font.render((str(msg)), 1, (255, 255, 255))
-    screen.blit(opening, (x + 5, y))
+def test():
+    print('test')
+
+
+tB = sgc.InputBox(pos=(100, 100), flags=1)
+tB.add(0)
+
+testt = 'test'
+fontB = pygame.font.SysFont('arial', 25, True)
+hz = fontB.render(testt, 1, (0, 0, 0))
+
+
+
+
+def redraw():
+    screen.fill((128, 128, 128))
+    sgc.update(clock.tick(userFPS))
+    fontB = pygame.font.SysFont('arial', 25, True)
+    hz = fontB.render(testt, 1, (0, 0, 0))
+    screen.blit(hz, (300, 5))
+    pygame.display.flip()
+
+
+run = True
+while run:
+    clock.tick(userFPS)
+    mPos = pygame.mouse.get_pos()
+    trueFPS = clock.get_fps()
+
+    for event in pygame.event.get():
+        sgc.event(event)
+        if event.type == GUI:
+            if event.text:
+                print(event)
+                testt = event.text
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            clicked = True
+        elif event.type == pygame.MOUSEBUTTONUP:
+            clicked = False
+            lClicked = False
+            rClicked = False
+            clickCount = 0
+
+
+        # Makes the window resizable
+        if event.type == pygame.VIDEORESIZE:
+            screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
+
+        if event.type == pygame.KEYUP:
+            # Edit mode
+            if event.key == pygame.K_e and not edit:
+                edit = True
+            elif event.key == pygame.K_e and edit:
+                edit = False
+            # Pause sim
+            if event.key == pygame.K_SPACE and not pause:
+                pause = True
+            elif event.key == pygame.K_SPACE and pause:
+                pause = False
+
+    redraw()
+
+pygame.quit()
+
 
